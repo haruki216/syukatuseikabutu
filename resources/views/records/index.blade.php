ad9110f266a85a49ef60da7c261e904ed4b634ae
haruki216 <harukimiyauchi@yahoo.co.jp>
2025-02-22 14:26:18 +0900

    チャット非表示

--- resources/views/records/index.blade.php
+++ resources/views/records/index.blade.php
@@ -10,7 +10,7 @@
         <li><a href="/calender">記録</a></li>
         <li><a href="/timer">タイマー</a></li>
         <li><a href="/calories">体重記録</a></li>
-        <li><a href="/post">チャット</a></li>
+      
          <li><a href="/gemini">AI</a></li>
         </ul>
     </nav>
